33546a31f0cacf5efb5de1785d72b399fda36589
brentwhistler <brent@brentwhistler.com>
2025-08-12 16:12:56 -0600

    caching review pulls

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration for Rezoort Website
 const CONFIG = {
     // Replace this with your actual Google Apps Script URL after deployment
-    GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxB8gyIBwkN3RAUb0aBpUJ8bNBfoDcsACFdciUdOnfx3TiXhVzo4FZ9K0mRlYXBsCgDOg/exec',
+    GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz5bx4PAbBYUMnbQRm-CzkE3U8lXa7lVk05qb0SIb02Jfp16oIMyp7Gnn5kvMvDp5u3_g/exec',
     
     // Google Place ID for Rezoort
     PLACE_ID: 'ChIJ__-DoiZBw4URdYMkjBu2ykM'
